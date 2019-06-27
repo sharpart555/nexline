e497e468ce2dfd53b64db0709ecea2c33e2c1672
SharpART <jason555@naver.com>
2019-06-28 00:46:11 +0900

    Update verify and sanitize logic

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const commonUtil = require('./util/commonUtil');
 /**
  * Create nextline
  * @param param
- * @param param.input text or buffer (it also can be array)
+ * @param param.input string or Readable stream
  * @param [param.lineSeperator] if not specified, auto detect crlf and lf
  */
 function nextline(param) {
@@ -15,34 +15,21 @@ function nextline(param) {
 	 * Verify & sanitize parameter
 	 */
 	const param2 = {
-		lineSeperator: undefined,
+		lineSeperator: ['\n', '\r\n'],
 		...param,
 	};
 
 	// Verify input
-	if (!param2.input) throw new Error('Empty input');
+	const input = param2.input;
+	if (!input) throw new Error('Empty input');
+	if (typeof input !== 'string' && !(input instanceof stream.Readable)) throw new Error('Invalid input. Input must be string or readable stream');
 
-	// Make input to array
-	const inputList = Array.isArray(param2.input) ? param2.input : [param2.input];
-	if (inputList.length === 0) throw new Error('Empty input');
-
-	for (const input of inputList) {
-		if (typeof input !== 'string' && !(input instanceof stream.Readable)) throw new Error('Invalid input. Input must be string or readable stream');
+	// Verify lineSeperator
+	const lineSeperatorList = Array.isArray(param2.lineSeperator) ? [...param2.lineSeperator] : [param2.lineSeperator];
+	if (lineSeperatorList.length === 0) throw new Error('Invalid lineSeperator');
+	for (const item of lineSeperatorList) {
+		if (typeof item !== 'string' || item.length === 0) throw new Error('Invalid lineSeperator, lineSeperator must be string and must exceed one character');
 	}
-
-	// Sanitize readSize
-	param2.readSize = commonUtil.sanitizeNumber({
-		value: param2.readSize,
-		default: 1024 * 1024,
-		min: 1,
-		max: 1024 * 1024 * 1024,
-	});
-
-	// Sanitize lineSeperator
-	param2.lineSeperator = commonUtil.sanitizeEnum({
-		value: param2.lineSeperator.toUpperCase(),
-		list: LINE_SEPERATOR_LIST,
-	});
 }
 
 /**
