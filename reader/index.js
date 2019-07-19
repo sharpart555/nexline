42ac91e6a03efde09eecc92a6f13b5171c741e71
SharpART <jason555@naver.com>
2019-07-20 00:56:16 +0900

    Add reader selection logic

--- reader/index.js
+++ reader/index.js
@@ -1,8 +1,27 @@
 /**
  * Import
  */
+const code = require('../code/code');
+const commonUtil = require('../util/commonUtil');
+const bufferReader = require('./bufferReader');
 const streamReader = require('./streamReader');
 
+/**
+ * Variable
+ */
+const { INPUT_TYPE } = code;
+
+/**
+ * Create inputReader
+ * @param input
+ */
+function create(input) {
+	const inputType = commonUtil.getInputType(input);
+	if (inputType === INPUT_TYPE.STREAM) return streamReader.create(input);
+	else if (inputType === INPUT_TYPE.STRING || inputType === INPUT_TYPE.BUFFER) return bufferReader.create(input);
+	else throw new Error('Invalid input');
+}
+
 module.exports = {
-	create: streamReader.create,
+	create,
 };
