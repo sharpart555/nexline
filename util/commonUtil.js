728f8f3697ea3128979e035c95b9489eae84dd87
SharpART <jason555@naver.com>
2019-07-20 09:56:25 +0900

    Update findIndexFromBuffer to handle multiple needle

--- util/commonUtil.js
+++ util/commonUtil.js
@@ -110,52 +110,81 @@ function removeUndefined(object) {
  * Find index from array of buffer
  * @param param
  * @param param.bufferList
- * @param param.needle
+ * @param param.needleList
  * @param [param.reverse]
  * @param [param.partial]
  */
 function findIndexFromBuffer(param) {
 	const param2 = {
+		bufferList: [],
+		needleList: [],
 		reverse: false,
 		partial: false,
 		...param,
 	};
-	const { bufferList, needle, reverse, partial } = param2;
+	const { bufferList, needleList, reverse, partial } = param2;
+	const result = { index: -1, size: 0, partial: false };
 
-	let totalBufferLength = 0;
-	for (const buffer of bufferList) totalBufferLength += buffer.length;
+	// Iterate over needleList
+	for (const needle of param2.needleList) {
+		const indexInfo = _findOne({ bufferList, needle, reverse, partial });
 
-	let baseIndex = 0;
-	let index = -1;
-	let size = 0;
-
-	for (let i = 0; i < bufferList.length; i++) {
-		const bufIdx = reverse ? bufferList.length - 1 - i : i;
-		const buffer = bufferList[bufIdx];
-
-		for (let j = 0; j < buffer.length; j++) {
-			const byteIdx = reverse ? buffer.length - 1 - j : j;
-			const needleIdx = reverse ? needle.length - 1 - size : size;
-
-			const bufferByte = buffer[byteIdx];
-			const needleByte = needle[needleIdx];
-
-			if (bufferByte === needleByte) {
-				if (size === 0) index = baseIndex + j;
-				size++;
-				if (size >= needle.length) {
-					return reverse ? { index: totalBufferLength - index - size, size } : { index, size };
-				}
-			} else {
-				index = -1;
-				size = 0;
-			}
+		if (
+			result.index === -1 || //
+			result.index > indexInfo.index ||
+			(result.index === indexInfo.index && result.size < indexInfo.size)
+		) {
+			result.index = indexInfo.index;
+			result.size = indexInfo.size;
+			result.partial = indexInfo.size !== needle.length;
 		}
-		baseIndex += buffer.length;
 	}
 
-	if (!partial) return { index: -1, size: 0 };
-	else return reverse && index !== -1 ? { index: totalBufferLength - index - size, size } : { index, size };
+	return result;
+
+	/**
+	 * Find index for one needle
+	 * @param param
+	 * @private
+	 */
+	function _findOne(param) {
+		const { bufferList, needle, reverse, partial } = param;
+
+		let totalBufferLength = 0;
+		for (const buffer of bufferList) totalBufferLength += buffer.length;
+
+		let baseIndex = 0;
+		let index = -1;
+		let size = 0;
+
+		for (let i = 0; i < bufferList.length; i++) {
+			const bufIdx = reverse ? bufferList.length - 1 - i : i;
+			const buffer = bufferList[bufIdx];
+
+			for (let j = 0; j < buffer.length; j++) {
+				const byteIdx = reverse ? buffer.length - 1 - j : j;
+				const needleIdx = reverse ? needle.length - 1 - size : size;
+
+				const bufferByte = buffer[byteIdx];
+				const needleByte = needle[needleIdx];
+
+				if (bufferByte === needleByte) {
+					if (size === 0) index = baseIndex + j;
+					size++;
+					if (size >= needle.length) {
+						return reverse ? { index: totalBufferLength - index - size, size } : { index, size };
+					}
+				} else {
+					index = -1;
+					size = 0;
+				}
+			}
+			baseIndex += buffer.length;
+		}
+
+		if (!partial) return { index: -1, size: 0 };
+		else return reverse && index !== -1 ? { index: totalBufferLength - index - size, size } : { index, size };
+	}
 }
 
 module.exports = {
