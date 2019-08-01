c09f1c893688e4d542c7dfb34c86ea42b9be9606
SharpART <jason555@naver.com>
2019-08-01 21:41:08 +0900

    Apply prettier to prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,10 @@
-// prettier override configuration for y-part
-// https://wiki.linecorp.com/pages/viewpage.action?spaceKey=LINEFE&title=Convention+TF+-+04.Prettier+guide
-// https://prettier.io/docs/en/options.html
-
 module.exports = {
-  printWidth: 200,
-  tabWidth: 4,
-  useTabs: true,
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'es5',
-  bracketSpacing: true,
-  arrowParens: 'always',
+	printWidth: 200,
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	arrowParens: 'always',
 };
