056e83ec2b9b1dc326d6442887c620c2654a8860
SharpART <jason555@naver.com>
2021-01-02 11:06:39 +0900

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,22 @@
-import { ReadStream } from 'fs';
-type NexlineInput = number | ReadStream | string | Buffer;
-
-declare function nexline(option: {
-  input: NexlineInput | NexlineInput[];
-  lineSeparator?: string | string[];
-  encoding?: string;
-  reverse?: boolean;
-  autoCloseFile?: boolean;
-}): {
-  next: () => Promise<string | null>;
-  close: () => void;
+declare const nexline: {
+  (param: nexline.NexlineOption): nexline.NexlineMethod;
 };
 
-export default nexline;
+declare namespace nexline {
+  import { ReadStream } from 'fs';
+  type NexlineInput = number | ReadStream | string | Buffer;
+  interface NexlineOption {
+    input: NexlineInput | NexlineInput[];
+    lineSeparator?: string | string[];
+    encoding?: string;
+    reverse?: boolean;
+    autoCloseFile?: boolean;
+  }
+
+  interface NexlineMethod {
+    next: () => Promise<string | null>;
+    close: () => void;
+  }
+}
+
+export = nexline;
